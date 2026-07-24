4ea2630d1fbc771001642b136097f1fe54111ee8
Jhuan067 <jhuanjesus6@gmail.com>
2026-07-23 23:37:03 -0300

    Base do header montada

---
+++ src/constants/navigation.js
@@ -0,0 +1,18 @@
+export const navigation = [
+  {
+    title: "Sobre",
+    href: "#about",
+  },
+  {
+    title: "Projetos",
+    href: "#projects",
+  },
+  {
+    title: "Experiência",
+    href: "#experience",
+  },
+  {
+    title: "Contato",
+    href: "#contact",
+  },
+];
